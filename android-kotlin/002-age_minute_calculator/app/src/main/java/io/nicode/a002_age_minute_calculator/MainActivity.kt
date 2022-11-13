e9bb36a9e1eee573112db0eeec5e4aa0ae1c26b3
Nicolas Denoël <nicolas.denoel@domintell.com>
2022-11-13 10:30:22 +0100

    Age Minute Calculator (AMC) - Initial setup

---
+++ android-kotlin/002-age_minute_calculator/app/src/main/java/io/nicode/a002_age_minute_calculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.nicode.a002_age_minute_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
